8b65133f2fb66f98756e389cbf9ccaffe99b2d86
ParkSangSun <appleasz1028102@gmail.com>
2021-12-21 10:57:47 +0900

    add : admin page

---
+++ app/src/main/java/com/pss/djmw_android/view/main/AdminFragment.kt
@@ -0,0 +1,17 @@
+package com.pss.djmw_android.view.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.pss.djmw_android.R
+import com.pss.djmw_android.base.BaseFragment
+import com.pss.djmw_android.databinding.FragmentAdminBinding
+
+
+class AdminFragment : BaseFragment<FragmentAdminBinding>(R.layout.fragment_admin) {
+    override fun init() {
+
+    }
+}
