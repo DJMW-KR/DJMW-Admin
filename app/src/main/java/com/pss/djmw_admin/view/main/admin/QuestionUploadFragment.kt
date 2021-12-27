89adfcb9072378501e28bf53ffab0ce8860cede8
ParkSangSun <appleasz1028102@gmail.com>
2021-12-27 20:41:03 +0900

    create : 문제 만들기 (관리자)

---
+++ app/src/main/java/com/pss/djmw_admin/view/main/admin/QuestionUploadFragment.kt
@@ -0,0 +1,13 @@
+package com.pss.djmw_admin.view.main.admin
+
+
+import com.pss.djmw_admin.R
+import com.pss.djmw_admin.base.BaseFragment
+import com.pss.djmw_admin.databinding.FragmentQuestionBinding
+
+
+class QuestionUploadFragment : BaseFragment<FragmentQuestionBinding>(R.layout.fragment_question) {
+    override fun init() {
+
+    }
+}
